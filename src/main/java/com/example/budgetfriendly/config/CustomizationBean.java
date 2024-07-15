68dd09d470c46525b246d2821d2e608046684cc2
Matlab <metleb.abbaszade@gmail.com>
2024-07-15 23:59:53 +0400

    index file moved

--- src/main/java/com/example/budgetfriendly/config/CustomizationBean.java
+++ src/main/java/com/example/budgetfriendly/config/CustomizationBean.java
@@ -1,25 +1,25 @@
-package com.example.budgetfriendly.config;
-
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.Ssl;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CustomizationBean implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-
-    @Override
-    public void customize(TomcatServletWebServerFactory factory) {
-        factory.setSsl(getSsl());
-        factory.setPort(8443);
-    }
-
-    private Ssl getSsl() {
-        Ssl ssl = new Ssl();
-        ssl.setKeyStore("classpath:path/to/your/keystore.p12");
-        ssl.setKeyStorePassword("your-password");
-        ssl.setKeyStoreType("PKCS12");
-        ssl.setKeyAlias("your-alias");
-        return ssl;
-    }
-}
+//package com.example.budgetfriendly.config;
+//
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.server.Ssl;
+//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class CustomizationBean implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+//
+//    @Override
+//    public void customize(TomcatServletWebServerFactory factory) {
+//        factory.setSsl(getSsl());
+//        factory.setPort(8443);
+//    }
+//
+//    private Ssl getSsl() {
+//        Ssl ssl = new Ssl();
+//        ssl.setKeyStore("classpath:path/to/your/keystore.p12");
+//        ssl.setKeyStorePassword("your-password");
+//        ssl.setKeyStoreType("PKCS12");
+//        ssl.setKeyAlias("your-alias");
+//        return ssl;
+//    }
+//}
